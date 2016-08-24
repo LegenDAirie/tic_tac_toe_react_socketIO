b64b1bf5cdc68313dbf923cc5032c9eba437c313
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-08-24 15:59:16 -0700

    add basic visible grid, add content change on click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import React from 'react';
+import Board from './board';
+import merge from 'lodash/merge';
 
-var App = React.createClass({
+// create button
+export default React.createClass({
+
+  getInitialState: function() {
+    return {
+      boardState: {
+        '00': '',
+        '01': '',
+        '02': '',
+        '10': '',
+        '11': '',
+        '12': '',
+        '20': '',
+        '21': '',
+        '22': ''
+      }
+    };
+  },
+
+  update: function(event) {
+
+    var obj1 = {};
+
+    if (this.state.boardState[event.target.id] == '') {
+      obj1[event.target.id] = 'x';
+    } else if (this.state.boardState[event.target.id] == 'x') {
+      obj1[event.target.id] = 'o';
+    } else if (this.state.boardState[event.target.id] == 'o') {
+      obj1[event.target.id] = '';
+    }
+
+    var newBoard = merge({}, this.state.boardState, obj1);
+
+    this.setState({
+      boardState: newBoard
+    });
+
+  },
 
   render: function() {
+    var style = { textAlign: 'center' };
+
     return (
-      <h1>Tic Tac Toe</h1>
+      <div>
+        <h1 style={style}>Tic Tac Toe</h1>
+        <Board position={this.state.boardState} clickHandler={this.update}/>
+      </div>
     );
   }
-});
 
-export default App;
+});
