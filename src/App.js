7ef4dead26d7c10b889a306234790dae83437bc9
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-08-29 18:51:26 -0700

    restart with working websockets

--- src/App.js
+++ src/App.js
@@ -1,55 +1,9 @@
-import React from 'react';
-import Board from './board';
-import merge from 'lodash/merge';
+import React, { Component } from 'react';
 
-// create button
-export default React.createClass({
-
-  getInitialState: function() {
-    return {
-      boardState: {
-        '00': '',
-        '01': '',
-        '02': '',
-        '10': '',
-        '11': '',
-        '12': '',
-        '20': '',
-        '21': '',
-        '22': ''
-      }
-    };
-  },
-
-  update: function(event) {
-
-    var obj1 = {};
-
-    if (this.state.boardState[event.target.id] == '') {
-      obj1[event.target.id] = 'x';
-    } else if (this.state.boardState[event.target.id] == 'x') {
-      obj1[event.target.id] = 'o';
-    } else if (this.state.boardState[event.target.id] == 'o') {
-      obj1[event.target.id] = '';
-    }
-
-    var newBoard = merge({}, this.state.boardState, obj1);
-
-    this.setState({
-      boardState: newBoard
-    });
-
-  },
-
-  render: function() {
-    var style = { textAlign: 'center' };
-
+export default class App extends Component {
+  render() {
     return (
-      <div>
-        <h1 style={style}>Tic Tac Toe</h1>
-        <Board position={this.state.boardState} clickHandler={this.update}/>
-      </div>
+      <h1> Hello world.</h1>
     );
   }
-
-});
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.scss';
-import './App.scss';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+var socket = io('http://localhost:1357');
+
+var form = document.getElementsByTagName('form')[0]
+var inputTag = document.getElementById('m');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  socket.emit('chat message', inputTag.value)
+  inputTag.value = '';
+});
+
+var messageList = document.getElementById('messages');
+
+socket.on('chat message', function(msg){
+  var li = document.createElement('li');
+  li.textContent = msg;
+  messageList.appendChild(li);
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
