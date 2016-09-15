b5402676a4a21adb61470c4049b310c3c6dfc71c
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-15 12:38:46 -0700

    Add testGameReducer file

---
+++ test/testGameReducer.js
@@ -0,0 +1,8 @@
+import expect from 'expect'
+import deepFreeze from 'deep-freeze'
+import boardReducer from '../src/stateManagement/reducers/boardReducer.js'
+import { describe, it } from 'mocha' // Not required, but shows where these functions come from
+
+describe('Board Reducer', function() {
+
+})
