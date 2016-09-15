52ab27064b8abfecdd494c38955338134a7c945d
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-15 15:05:42 -0700

    Change addPiece action creator to recieve current player as second argument

--- src/stateManagement/constants/actionTypes.js
+++ src/stateManagement/constants/actionTypes.js
@@ -4,10 +4,18 @@ export const SET_SCALE = 'SET_SCALE'
 export const RESET_BOARD = 'RESET_BOARD'
 export const SET_ACTIVE_PLAYER = 'SET_ACTIVE_PLAYER'
 export const GAME_OVER = 'GAME_OVER'
+export const PLAYERS = {
+  X: 'X',
+  O: 'O'
+}
 
 // Action Creators
-export function addPiece(position) {
-  return { type: ADD_PIECE, position }
+export function addPiece(position, currentPlayer) {
+  return {
+    type: ADD_PIECE,
+    piece: currentPlayer,
+    position
+  }
 }
 
 export function setScale(scale) {
