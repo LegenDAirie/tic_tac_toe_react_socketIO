148aa7dc989d0332209a5997261bdbb3897d07a6
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-16 16:47:48 -0700

    Calculate scale function

--- src/stateManagement/reducers/appReducer.js
+++ src/stateManagement/reducers/appReducer.js
@@ -1,4 +1,12 @@
+import { SET_SCALE } from '../constants/actionTypes.js'
 import gameReducer from './gameReducer.js'
+
+const calculateScale = ({ currentScale, action }) => {
+  if (action.type !==  SET_SCALE) {
+    return currentScale
+  }
+  return 1
+}
 
 const initialState = {
   scale: 1,
@@ -7,7 +15,7 @@ const initialState = {
 
 const appReducer = (state = initialState, action) => {
   return {
-    scale: 1,
+    scale: calculateScale({ currentScale: state.scale, action }),
     gameState: gameReducer(state.gameState, action)
   }
 }
