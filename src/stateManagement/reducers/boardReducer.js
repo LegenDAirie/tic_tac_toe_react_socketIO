a5c38a1c944be03849a33cf9cbced7e4704442c7
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-16 16:50:29 -0700

    Change state to boardState

--- src/stateManagement/reducers/boardReducer.js
+++ src/stateManagement/reducers/boardReducer.js
@@ -2,24 +2,24 @@ import { ADD_PIECE, RESET_BOARD } from '../constants/actionTypes.js'
 
 const initialState = ['', '', '', '', '', '', '', '', '']
 
-const boardReducer = (state = initialState, action) => {
+const boardReducer = (boardState = initialState, action) => {
   const { position, piece, type } = action
 
   switch (type) {
     case ADD_PIECE:
-      if ( state[position] !== '' ) {
+      if ( boardState[position] !== '' ) {
 
-        return state
+        return boardState
       }
-      var beginningArray = state.slice(0, position)
-      var endingArray = state.slice(position+1, state.length)
+      var beginningArray = boardState.slice(0, position)
+      var endingArray = boardState.slice(position+1, boardState.length)
       return beginningArray.concat([piece]).concat(endingArray)
 
     case RESET_BOARD:
       return ['', '', '', '', '', '', '', '', '']
 
     default:
-      return state
+      return boardState
   }
 }
 
