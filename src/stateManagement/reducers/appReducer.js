6cea720b26dec34b5225f689017b7e93b77bb6f5
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-16 16:37:02 -0700

    Replace switch statement with return statement

--- src/stateManagement/reducers/appReducer.js
+++ src/stateManagement/reducers/appReducer.js
@@ -1,4 +1,3 @@
-import { ADD_PIECE, RESET_BOARD } from '../constants/actionTypes.js'
 import gameReducer from './gameReducer.js'
 
 const initialState = {
@@ -7,15 +6,9 @@ const initialState = {
 }
 
 const appReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_PIECE:
-      return state
-
-    case RESET_BOARD:
-      return state
-
-    default:
-      return state
+  return {
+    scale: 1,
+    gameState: gameReducer(state.gameState, action)
   }
 }
 
