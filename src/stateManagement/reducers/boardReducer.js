f43a32b8f8327bdacb17a0acb6ef97d797614752
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-15 17:12:18 -0700

    Add explicit return of empty game board back in pass a test case

--- src/stateManagement/reducers/boardReducer.js
+++ src/stateManagement/reducers/boardReducer.js
@@ -6,18 +6,17 @@ const boardReducer = (state, action) => {
 
   switch (type) {
     case ADD_PIECE:
-
       if ( state[position] !== '' ) {
+
         return state
       }
-
       var beginningArray = state.slice(0, position)
       var endingArray = state.slice(position+1, state.length)
-
       return beginningArray.concat([piece]).concat(endingArray)
 
-
     case RESET_BOARD:
+      return ['', '', '', '', '', '', '', '', '']
+      
     default:
       return state
   }
