3f473cd2ed333ee52dd024f61d992e0b6a69cdf7
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-17 14:40:17 -0700

    Rename fill-rule to fullRule

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -18,7 +18,7 @@ var GameBoard = function(props) {
         <title>
           TicTacToe Board
         </title>
-        <g fill="none" fill-rule="evenodd">
+        <g fill="none" fillRule="evenodd">
           <path fill="#BABABA" d="M101 300h-2V0h2m100 300h-2V0h2"/>
           <path fill="#B9B9B9" d="M0 99h300v2H0zm0 100h300v2H0z"/>
         </g>
