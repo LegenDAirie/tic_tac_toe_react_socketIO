87495fe21958d3cba673c96d44dc831c0ac0703a
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-17 14:18:14 -0700

    Remove pieces and pices creating logic

--- src/game.js
+++ src/game.js
@@ -1,57 +1,9 @@
 import React from 'react'
 import GameBoard from './gameBoard'
-import PieceX from './pieceX'
-import PieceO from './pieceO'
-import BoardCell from './boardCell'
-
-var placePieces = function(store) {
-  var boardCells = store.getState()
-  var currentCells = []
-  var scale = 1
-  var cellWidth = 100
-  var cellHeight = cellWidth
-
-  for (var rowNum = 0; rowNum < 3; rowNum++) {
-    for (var colNum = 0; colNum < 3; colNum++) {
-      var width = scale * cellWidth - 1
-      var left = scale * colNum * cellWidth
-      var top = scale * rowNum * cellHeight
-      let position = (rowNum*3) + colNum
-      var identifier = '' + rowNum + colNum
-
-      var Piece = BoardCell
-
-      if ( boardCells[rowNum*3 + colNum] === 'X' ) {
-        Piece = PieceX
-      }
-
-      if ( boardCells[rowNum*3 + colNum] === 'O' ) {
-        Piece = PieceO
-      }
-
-      currentCells.push(
-        <Piece id={identifier} key={identifier} store={store}
-          width={width} left={left} top={top}
-          handleCellSelect={function() {
-            store.dispatch({
-              type: 'ADD_PIECE',
-              position: position,
-              piece: 'X'
-            })
-          }}
-        />
-      )
-    }
-  }
-
-  return currentCells
-}
 
 const App = function (props) {
 
   var { store } = props
-
-  var pieces = placePieces(store)
 
   var style = {
     position: 'relative'
@@ -60,7 +12,6 @@ const App = function (props) {
   return (
     <div id="board-container" style={style}>
       <GameBoard/>
-      {pieces}
     </div>
   )
 }
