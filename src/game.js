19c3dd4493ddd1a906289ebb02695fc0812a3427
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-17 15:24:12 -0700

    Remove desctructuring

--- src/game.js
+++ src/game.js
@@ -2,18 +2,12 @@ import React from 'react'
 import GameBoard from './gameBoard'
 import Pieces from './pieces.js'
 
-const Game = function (props) {
-
-  var { store } = props
-
-  var style = {
-    position: 'relative'
-  }
-
+const Game = function ( props ) {
+  var style = { position: 'relative' }
   return (
-    <div id="board-container" style={style}>
+    <div id="board-container" style={ style }>
       <GameBoard/>
-      <Pieces store={store}/>
+      <Pieces store={ props.store }/>
     </div>
   )
 }
