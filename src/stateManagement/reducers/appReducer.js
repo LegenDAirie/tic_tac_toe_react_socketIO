028822461d306e52126bbb3c975d9578a336c4e1
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-16 17:54:15 -0700

    Change name to getScale

--- src/stateManagement/reducers/appReducer.js
+++ src/stateManagement/reducers/appReducer.js
@@ -1,7 +1,7 @@
 import { SET_SCALE } from '../constants/actionTypes.js'
 import gameReducer from './gameReducer.js'
 
-const calculateScale = ({ currentScale, action }) => {
+const getScale = ({ currentScale, action }) => {
   var { type, scale } = action
   if (type !== SET_SCALE) {
     return currentScale
@@ -16,7 +16,7 @@ const initialState = {
 
 const appReducer = (state = initialState, action) => {
   return {
-    scale: calculateScale({ currentScale: state.scale, action }),
+    scale: getScale({ currentScale: state.scale, action }),
     gameState: gameReducer(state.gameState, action)
   }
 }
