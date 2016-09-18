42ec8d5859d9c49d9397f157ceec4fec175407b7
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-17 17:24:17 -0700

    Check for winning victory

--- src/stateManagement/reducers/gameReducer.js
+++ src/stateManagement/reducers/gameReducer.js
@@ -4,8 +4,30 @@ const { X, O } = PLAYERS
 
 function checkIfWon(boardState, action) {
   const { position, piece } = action
-  // do some calculation
-  return false
+  const newBoard = boardReducer(boardState, action)
+  const winningConfigurations = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ]
+  const hasBeenWon = winningConfigurations.some(function(winningConfiguration) {
+    var XHasWon = winningConfiguration.every(function(position) {
+      var index = position - 1
+      return newBoard[index] === X
+    })
+    var OHasWon = winningConfiguration.every(function(position) {
+      var index = position - 1
+      return newBoard[index] === O
+    })
+
+    return XHasWon || OHasWon
+  })
+  return hasBeenWon
 }
 
 const initialState = {
