c9239883e567921acc6a5a395e7108803d0535cd
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-19 16:07:43 -0700

    Initialize App with proper scale

--- src/app.js
+++ src/app.js
@@ -14,11 +14,13 @@ var render = function render() {
   )
 }
 
-store.subscribe(render)
-render()
-
-window.addEventListener('resize', function() {
+var setAppScale = function() {
   var heightNeeded = window.innerHeight * 0.8
   var newScale = heightNeeded / 300 //height on the game grid
   store.dispatch(setScale(newScale))
-})
+}
+
+store.subscribe(render)
+setAppScale()
+
+window.addEventListener('resize', setAppScale)
