e07f9713e77d02f4e28bcfff6e5cf18f0f65cbe4
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-19 15:46:21 -0700

    Explain what 300 is

--- src/app.js
+++ src/app.js
@@ -19,6 +19,6 @@ render()
 
 window.addEventListener('resize', function() {
   var heightNeeded = window.innerHeight * 0.8
-  var newScale = heightNeeded / 300
+  var newScale = heightNeeded / 300 //height on the game grid
   store.dispatch(setScale(newScale))
 })
