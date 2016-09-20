c58fe19e2e85ef0d7c7a2ca270ee789e2632761f
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-19 17:24:09 -0700

    Add CurrentPlayer component to Game component

--- src/game.js
+++ src/game.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ResetButton from './resetButton'
 import GameBoard from './gameBoard'
+import CurrentPlayer from './currentPlayer'
 
 const Game = function ( props ) {
   var state = props.store.getState()
   var style = { position: 'relative' }
   return (
     <div id="board-container" style={ style }>
-      <ResetButton store={ props.store }/>
-      <GameBoard store={ props.store }/>
+      <ResetButton store={ props.store } />
+      <CurrentPlayer currentPlayer={state.gameState.currentPlayer} />
+      <GameBoard store={ props.store } />
     </div>
   )
 }
