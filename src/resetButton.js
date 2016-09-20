56f6c9677751880a35ae49fc86afd9d5c08d7e90
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-19 18:29:49 -0700

    Add basic styling to reset button

--- src/resetButton.js
+++ src/resetButton.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { resetBoard } from './stateManagement/actionTypes'
 
+var style = {
+  backgroundColor: '#f55',
+  color: 'white',
+  border: 'none',
+  textDecoration: 'none',
+  fontSize: '25px',
+  padding: '14px',
+  margin: '10px'
+}
+
 const ResetButton = props => (
   <div>
-    <button onClick={ () => props.store.dispatch(resetBoard()) }>
-      ResetGame
+    <button style={ style } onClick={ () => props.store.dispatch(resetBoard()) }>
+      Reset Game
     </button>
   </div>
 )
