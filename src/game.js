8f2e7c2f859fd6c5a194745c1321649c7e4f50a9
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-22 13:26:49 -0700

    Import styles from componentStyles file

--- src/game.js
+++ src/game.js
@@ -2,34 +2,10 @@ import React from 'react'
 import ResetButton from './resetButton'
 import GameBoard from './gameBoard'
 import CurrentPlayer from './currentPlayer'
+import { gameStyle, optionsStyle } from './reactCSS/componentStyles'
 
 const Game = function ( props ) {
   var state = props.store.getState()
-
-  var gameStyle = {
-    width: '45vw',
-    height: '95vw',
-    background: '#777',
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '350px',
-    minWidth: '300px',
-    maxHeight: '95vh',
-    maxWidth: '95vh',
-    alignItems: 'center'
-  }
-
-  var optionsStyle = {
-    display: 'flex',
-    flexGrow: 1,
-    backgroundColor: '#913',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    minHeight: '50px',
-    minWidth: '300px',
-    maxHeight: '14.25vh',
-    width: '100%',
-  }
 
   return (
     <div>
