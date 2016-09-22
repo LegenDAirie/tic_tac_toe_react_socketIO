124c7155e2a6d7d5d53dd33c925239b5149590ba
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-22 13:24:59 -0700

    Move game styles into componentStyles file

---
+++ src/reactCSS/componentStyles.js
@@ -0,0 +1,24 @@
+export const gameStyle = {
+  width: '45vw',
+  height: '95vw',
+  background: '#777',
+  display: 'flex',
+  flexDirection: 'column',
+  minHeight: '350px',
+  minWidth: '300px',
+  maxHeight: '95vh',
+  maxWidth: '95vh',
+  alignItems: 'center'
+}
+
+export const optionsStyle = {
+  display: 'flex',
+  flexGrow: 1,
+  backgroundColor: '#913',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  minHeight: '50px',
+  minWidth: '300px',
+  maxHeight: '14.25vh',
+  width: '100%',
+}
