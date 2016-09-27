532f819ca8f4ba97eac002f255e00966ff27d113
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-26 20:41:41 -0700

    Setup a basic http server

---
+++ server.js
@@ -0,0 +1,25 @@
+var http = require('http')
+var httpServer = http.createServer(handler)
+var fs = require('fs');
+
+httpServer.listen(8000, 'localhost');
+
+function serveUpFile(req, res, path) {
+  fs.readFile(path.toString(), function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200)
+    res.end(data)
+  })
+}
+
+function handler (req, res) {
+  if (req.url === '/static/bundle.js') {
+    serveUpFile(req, res, './static/bundle.js')
+  } else {
+    serveUpFile(req, res, './index.html')
+  }
+}
