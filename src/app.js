e3b6d0ec5cdd0a308980227f4ad98689608f9ce4
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-09-29 13:05:06 -0700

    Remove PeerJS

--- src/app.js
+++ src/app.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom'
 import createStore from './stateManagement/createStore'
 import appReducer from './stateManagement/reducers/appReducer'
 import Game from './game'
-import Peer from 'peerjs'
-
-var peer = new Peer('someid', {host: 'localhost', port: 9000, path: '/'})
-
-peer.on('connection', function () {
-  console.log('a peer has connected')
-})
 
 var store = createStore(appReducer)
 
