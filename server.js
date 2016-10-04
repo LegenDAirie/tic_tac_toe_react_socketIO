5ecd77e8cf75797675e5ea9c43f62aaf14a7ac2b
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-10-04 14:50:27 -0700

    Change name from handler to requiestHandler

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var http = require('http')
-var httpServer = http.createServer(handler)
+var httpServer = http.createServer(requestHandler)
 var fs = require('fs')
+var io = require('socket.io')(httpServer)
+var p2p = require('socket.io-p2p-server').Server
+io.use(p2p)
 
 httpServer.listen(8000, 'localhost');
 
@@ -16,10 +19,23 @@ function serveUpFile(req, res, path) {
   })
 }
 
-function handler (req, res) {
+function requestHandler (req, res) {
   if (req.url === '/static/bundle.js') {
     serveUpFile(req, res, './static/bundle.js')
   } else {
     serveUpFile(req, res, './index.html')
   }
 }
+
+io.on('connection', function (socket) {
+  console.log('client connected to the server')
+
+  socket.on('peer-msg', function (data) {
+    console.log('Message from peer %s', data)
+    socket.broadcast.emit('peer-msg', data)
+  })
+
+  socket.on('disconnect', function () {
+    console.log('client disconnected from the server')
+  })
+})
