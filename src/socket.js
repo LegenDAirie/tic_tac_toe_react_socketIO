848552ca5c9910a3638943e998bf68bffa06f1c0
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-10-14 14:11:51 -0700

    Move socket logic from app to here

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,32 @@
 import io from 'socket.io-client'
 const socket = io()
 export default socket
+
+socket.on('connected', function (data) {
+  console.log(data)
+  socket.ZZZk
+})
+
+socket.on('myPiece', function (piece) {
+  console.log('myPiece is ' + piece)
+})
+
+socket.on('hello', function (data) {
+  console.log(data)
+})
+
+window.gtfo = function () {
+  socket.emit('leave room')
+}
+
+window.sayHi = function () {
+  socket.emit('hello', "friend!")
+}
+
+window.getStatusPlz = function () {
+  socket.emit('status')
+}
+
+window.findFriend = function () {
+  socket.emit('join room')
+}
