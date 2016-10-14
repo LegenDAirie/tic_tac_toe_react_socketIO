ee22e1547df2602d6b7b59c05ef6673f1fdde12c
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-10-14 14:08:45 -0700

    Move socket logic into it's own file

--- src/app.js
+++ src/app.js
@@ -1,46 +1,19 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import createStore from './stateManagement/createStore'
-// import appReducer from './stateManagement/reducers/appReducer'
-// import Game from './game'
-import io from 'socket.io-client'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import socket from './socket'
+import createStore from './stateManagement/createStore'
+import appReducer from './stateManagement/reducers/appReducer'
+import Game from './game'
 
-const socket = io()
 
-console.log(socket.id)
+var store = createStore(appReducer)
 
-socket.on('connected', function (data) {
-  console.log(data)
-})
-
-socket.on('hello', function (data) {
-  console.log(data)
-})
-
-window.gtfo = function () {
-  socket.emit('leave room')
+var render = function render() {
+  ReactDOM.render(
+    <Game store={ store }/>,
+    document.getElementById('root')
+  )
 }
 
-window.sayHi = function () {
-  socket.emit('hello', "friend!")
-}
-
-window.getStatusPlz = function () {
-  socket.emit('status')
-}
-
-window.findFriend = function () {
-  socket.emit('join room')
-}
-
-// var store = createStore(appReducer)
-//
-// var render = function render() {
-//   ReactDOM.render(
-//     <Game store={ store }/>,
-//     document.getElementById('root')
-//   )
-// }
-//
-// store.subscribe(render)
-// render()
+store.subscribe(render)
+render()
