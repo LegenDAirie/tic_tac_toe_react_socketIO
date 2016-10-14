0a21901854406a9b9bfddc5de44da48c76b897a5
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-10-14 16:25:18 -0700

    Move socket back into app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import socket from './socket'
 import createStore from './stateManagement/createStore'
 import appReducer from './stateManagement/reducers/appReducer'
 import Game from './game'
+import setPlayerPiece from './stateManagement/actionTypes'
+import io from 'socket.io-client'
+const socket = io()
 
+socket.on('connected', function (data) {
+  // waiting for other player to connect screen
+})
+
+socket.on('start game', function (piece) {
+  store.dispatch(setPlayerPiece(piece))
+})
+
+window.gtfo = function () {
+  socket.emit('leave room')
+}
+
+window.getStatusPlz = function () {
+  socket.emit('status')
+}
+
+window.findFriend = function () {
+  socket.emit('join room')
+}
 
 var store = createStore(appReducer)
 
