0951ba8c3709b1dff7e377669675fb5694df1b5d
AIRIEL D. SALVATORE <AIRIEL.D.SALVATORE@GMAIL.COM>
2016-10-14 19:06:36 -0700

    Display who wins

--- src/game.js
+++ src/game.js
@@ -5,25 +5,17 @@ import CurrentPlayer from './currentPlayer'
 import { gameStyle, optionsStyle } from './reactCSS/componentStyles'
 import { socket } from './socket'
 
-const Game = function ( props ) {
-  var state = props.state
-  console.log('currentPlayer: ', state.gameState.currentPlayer)
-  console.log('your piece: ', socket.piece)
-  var whichTurn = state.gameState.currentPlayer === socket.piece ? 'yours': 'there\'s'
-  whichTurn = socket.piece === '' ? 'waiting on player to connect' : 'Turn: ' + whichTurn
-
-  return (
-    <div>
-      <div id="board-container" style={ gameStyle }>
-        <div style={ optionsStyle }>
-          <ResetButton/>
-          <CurrentPlayer currentPlayer={ whichTurn } />
-        </div>
-        <GameBoard state={ state } />
+const Game = props => (
+  <div>
+    <div id="board-container" style={ gameStyle }>
+      <div style={ optionsStyle }>
+        <ResetButton />
+        <CurrentPlayer state= { props.state }/>
       </div>
+      <GameBoard state={ props.state } />
     </div>
-  )
-}
+  </div>
+)
 
 Game.propTypes = {
   state: React.PropTypes.object.isRequired
--- src/resetButton.js
+++ src/resetButton.js
@@ -2,12 +2,19 @@ import React from 'react'
 import { resetButtonStyle } from './reactCSS/componentStyles'
 import { socket } from './socket'
 
-const ResetButton = () => (
-  <div>
-    <button style={ resetButtonStyle } onClick={ () => socket.emit('reset board') }>
-      Reset Game
-    </button>
-  </div>
-)
+const ResetButton = () => {
+  var joinRoom = () => socket.emit('join room')
+  var resetBoard = () => socket.emit('reset board')
+  var buttonAction = socket.piece === '' ? joinRoom : resetBoard
+  var buttonText = socket.piece === '' ? 'Join Room' : 'Reset Game'
+
+  return (
+    <div>
+      <button style={ resetButtonStyle } onClick={ buttonAction }>
+        { buttonText }
+      </button>
+    </div>
+  )
+}
 
 export default ResetButton
